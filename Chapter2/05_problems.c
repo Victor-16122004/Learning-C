c0cf398eb3131a4aa3806f5c90fab168a83ed040
Victor-16122004 <ashishxyz557@gmail.com>
2025-07-16 19:23:33 +0530

    Chapter 2

---
+++ Chapter2/05_problems.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    float a =3.0 + 1;
+    printf("the value of a is %f/n",a);
+    return 0;
+}
---
+++ Chapter2/05_typecasting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int n = 45;
+    int m = 32.23;
+
+    n = (int) m; // convert the data type to int
+    printf("The value of n is %d\n", n);
+    return 0;
+}
